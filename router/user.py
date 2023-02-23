f7b537224e34282ab7f5e3efad6eb0777d1d53d8
KDBlueMoon <126132042+KDBlueMoon@users.noreply.github.com>
2023-02-23 15:06:34 +0700

    Update user.py

--- router/user.py
+++ router/user.py
@@ -3,6 +3,7 @@ from model.user import User
 from schemas import user as userschemas
 from typing import List
 from playhouse.shortcuts import model_to_dict
+from schemas.login import Login
 
 router = APIRouter()
 
@@ -13,3 +14,15 @@ def get_account():
 @router.post('/create')
 def create_account(user:userschemas.UserCreate):
     return model_to_dict(User.create(**user.dict()))
+
+@router.post('/login')
+def sign_in(user:Login):
+    """User login"""
+    user = user.dict()
+    query = User.select().where(
+        (User.code == user['code']) & (User.password == user['password'])).dicts()
+    query_len = list(query)
+    if len(query_len):
+        return {"code": 200, "data": query_len}
+    else:
+        return {"code": 400, "data": query_len}
